d67f2e5745c8738f8caf47ae7bdbf0eb95baaa6b
Arvin <arvinflash@gmail.com>
2021-06-19 16:03:01 +0430

    Target sdk 30 fix

--- buildSrc/src/main/java/BuildConfiguration.kt
+++ buildSrc/src/main/java/BuildConfiguration.kt
@@ -1,12 +1,12 @@
 @file:Suppress("KDocMissingDocumentation")
 
 object BuildConfiguration {
-    const val targetSdkVersion = 29
-    const val compileSdkVersion = 29
+    const val targetSdkVersion = 30
+    const val compileSdkVersion = 30
     const val minSdkVersion = 21
 
-    const val versionCode = 1
-    const val versionName = "0.1.0"
+    const val versionCode = 2
+    const val versionName = "0.2.3"
 
     const val applicationId = "com.aceinteract.android.stepper"
 }
